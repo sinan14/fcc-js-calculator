c9556726c56ad23da0c73baa4903a0476d1382e7
sinan14 <sinuzar5@gmail.com>
2023-05-25 22:55:20 +0530

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="fcc-js-calculator" />
+    </div>
+  );
+}
+
+export default App;
